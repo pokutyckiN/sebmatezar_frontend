65e3e01ce57e9c7e441f53304e20e02a008aaca0
Nazar Pokutycki <245596@student.pwr.edu.pl>
2022-05-28 23:31:48 +0200

    Creating project

---
+++ src/components/Start.vue
@@ -0,0 +1,36 @@
+<template>
+    <img class="logo-app" src="../assets/sebmatezar.jpg">
+  <div>
+    <h1>SEBMATEZAR</h1>
+    <input class="form-control search-crime" type="text" placeholder="auto theft">
+    <router-link to="/search">
+        <button type="button" class="btn btn-primary" style="margin: 10px;">Search</button>
+    </router-link>
+    <p class="search-engine-info">
+        A search engine dedicated to finding information about crimes commited in Atlanta from 2009 to 2017
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartPage',
+}
+</script>
+
+<style scoped>
+.logo-app {
+    height: 300px;
+    width: auto;
+}
+.search-crime {
+  width: 500px;
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+}
+.search-engine-info {
+    font-weight: bold; 
+    margin-top: 150px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
